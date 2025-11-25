764266117f2bcb24b2baac48447386b6ffd7b0f2
Aine <aaluowan@gmail.com>
2025-11-24 20:05:49 -0800

    fixed span issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,39 +36,41 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="h-screen bg-white grid grid-cols-12 relative overflow-hidden">
-      {/* Static left side - content spans 4 columns */}
-      <div className="col-span-5 p-5 relative">
-        <div className="flex flex-col gap-4" style={{ width: 'calc(100vw * 4 / 12)' }}>
-          <h2>Alex is a digital product designer making technology feel human, by designing products that connect with people, not just serve them.</h2>
-          <div className="subheader">Designer @AthenaHQ (YC W25)</div>
+    <div className="h-screen bg-white flex justify-center overflow-hidden">
+      <div className="w-full max-w-[1600px] grid grid-cols-12 relative h-full">
+        {/* Static left side - content spans 4 columns */}
+        <div className="col-span-4 p-5 relative">
+          <div className="flex flex-col gap-4">
+            <h2>Alex is a digital product designer making technology feel human, by designing products that connect with people, not just serve them.</h2>
+            <div className="subheader">Designer @AthenaHQ (YC W25)</div>
+          </div>
+          <nav className="absolute bottom-5 left-5 flex flex-row gap-6">
+            <a href="#" className="subheader">PLAYGROUND</a>
+            <a href="#" className="subheader">ABOUT</a>
+            <a href="#" className="subheader">CV</a>
+          </nav>
         </div>
-        <nav className="absolute bottom-5 left-5 flex flex-row gap-6">
-          <a href="#" className="subheader">PLAYGROUND</a>
-          <a href="#" className="subheader">ABOUT</a>
-          <a href="#" className="subheader">CV</a>
-        </nav>
-      </div>
-      
-      {/* Scrollable right side for case studies - spans 7 columns */}
-      <div 
-        ref={scrollContainerRef}
-        className="col-span-7 h-screen overflow-y-auto overflow-x-hidden"
-      >
-        <div className="scroll-content p-5 space-y-5">
-          {/* Project Cards */}
-          <ProjectCard
-            mediaSrc="/projects/88rising/videos/88risingthumbnail.mp4"
-            mediaType="video"
-            badges={["Branding", "Design"]}
-            className="hover-target-big"
-          />
-          <ProjectCard
-            mediaSrc="/projects/blueberry-social/videos/blueberrysizzle.mp4"
-            mediaType="video"
-            badges={["Social Media", "Content"]}
-            className="hover-target-big"
-          />
+        
+        {/* Scrollable right side for case studies - spans 8 columns */}
+        <div 
+          ref={scrollContainerRef}
+          className="col-span-8 h-screen overflow-y-auto overflow-x-hidden scrollbar-hide"
+        >
+          <div className="scroll-content p-5 space-y-5">
+            {/* Project Cards */}
+            <ProjectCard
+              mediaSrc="/projects/88rising/videos/88risingthumbnail.mp4"
+              mediaType="video"
+              badges={["88rising", "Product Design"]}
+              className="hover-target-big"
+            />
+            <ProjectCard
+              mediaSrc="/projects/blueberry-social/videos/blueberrysizzle.mp4"
+              mediaType="video"
+              badges={["88rising", "Product Design"]}
+              className="hover-target-big"
+            />
+          </div>
         </div>
       </div>
     </div>
